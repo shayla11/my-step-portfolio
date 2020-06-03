ebd95ad7f87e4f06fd19288c6c72ebdc172982e1
Shayla <shaylae116@gmail.com>
2020-06-03 00:07:36 +0000

    edit doGet function

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -19,20 +19,30 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import com.google.gson.Gson;
+
 
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
 
-    ArrayList<String> comments = new ArrayList<String>();
-    comments.add("Why does your word insure me like its Aflac");
-    comments.add("I know shortcuts cut short long runs");
-    comments.add("Livin' la vida without the loca");
-    comments.add("front row? Duh bro we dont sit in nose bleeds");
+    private ArrayList<String> comments;
+
+    @Override
+    public void init() {
+        comments = new ArrayList<String>();
+        comments.add("Why does your word insure me like its Aflac");
+        comments.add("I know shortcuts cut short long runs");
+        comments.add("Livin' la vida without the loca");
+        comments.add("Front row? Duh bro we dont sit in nose bleeds");
+    }
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello Shayla!</h1>");
+    Gson gson = new Gson();
+    String jsonComments = gson.toJson(comments);
+    response.setContentType("application/json;");
+    response.getWriter().println(jsonComments);
   }
 }
