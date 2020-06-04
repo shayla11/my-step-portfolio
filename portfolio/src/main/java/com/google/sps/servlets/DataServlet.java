41e33908c4bf0f6bce5ebdfeaeb7b5253ce46616
Shayla <shaylae116@gmail.com>
2020-06-04 22:06:50 +0000

    task and save comments

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -19,11 +19,15 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
 import java.util.ArrayList;
 import com.google.gson.Gson;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
+import com.google.sps.data.Task;
 
 
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
@@ -35,6 +39,20 @@ public class DataServlet extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         Gson gson = new Gson();
+
+        Query query = new Query("Task").addSort("timestamp", SortDirection.DESCENDING);
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        PreparedQuery results = datastore.prepare(query);
+        ArrayList<Task> tasks = new ArrayList<Task>();
+	    for (Entity entity : results.asIterable()) {
+        	long id = entity.getKey().getId();
+      	    String text = (String) entity.getProperty("text");
+      	    long timestamp = (long) entity.getProperty("timestamp");
+
+      	    Task task = new Task(id, text, timestamp);
+      	    tasks.add(task);
+        }
+
         String jsonComments = gson.toJson(comments);
         response.setContentType("application/json;");
         response.getWriter().println(jsonComments);
