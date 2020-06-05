f7d3601204313c3ea434815d8a7b55fc4074bff4
Shayla <shaylae116@gmail.com>
2020-06-05 21:25:19 +0000

    add todo statement

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -41,6 +41,8 @@ async function getComments() {
   }
 }
 
+//TODO: Implement a method to delete comments
+
 function createListElement(text) {
     const liElement = document.createElement('li-comments');
     liElement.innerText = text;
