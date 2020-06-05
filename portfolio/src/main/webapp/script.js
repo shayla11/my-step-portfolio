64f97f78de531be76b93a645161743f2808f9009
Shayla <shaylae116@gmail.com>
2020-06-05 14:16:52 +0000

    comment display

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -29,7 +29,7 @@ function addRandomMovieQuote() {
 
 async function getRandomMessage() {
     fetch('/data').then(response => response.json()).then((tasks) => {
-        const messageList = document.getElementById('quote-container');
+        const messageList = document.getElementById('comments-container');
         messageList.innerHTML = '';
         for (let i = 0; i < tasks.length; i++) {
             messageList.appendChild(createListElement(tasks[i].text));
