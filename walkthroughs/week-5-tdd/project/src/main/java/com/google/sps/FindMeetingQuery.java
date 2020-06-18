4de87e6b98a14ef522c1658a8ccf9700b250f8b5
Shayla <shaylae116@gmail.com>
2020-06-18 23:22:46 +0000

    fix comments, spacing and variables

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -20,65 +20,65 @@ import java.util.Collection;
 
 public final class FindMeetingQuery {
 
-    public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
+  public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
         
-        //Collects data from the request: meeting duration and the attendees
-        int duration = (int) request.getDuration();
-        Collection<String> attendees = request.getAttendees();
-        //Collection<String> opAttendees = request.getOptionalAttendees();
+      // Collects data from the request: meeting duration and the attendees
+      int duration = (int) request.getDuration();
+      Collection<String> attendees = request.getAttendees();
 
-        //This is the collection of time ranges that we willl return that fits everyones schedule
-        Collection<TimeRange> availableTimes = new ArrayList<>();
-        Collection<TimeRange> unavailableTimes = new ArrayList<>();
+      // This is the collection of time ranges that we will return that fits everyones schedule.
+      Collection<TimeRange> availableTimes = new ArrayList<>();
+      Collection<TimeRange> potentialTimes = new ArrayList<>();
 
-        int startTime = 0;
-        int endTime = startTime + duration;
+      int startTime = 0;
+      int endTime = startTime + duration;
+      
+      // Cycles through the day, incrementally to check the potential time range.
 
-        while( startTime < TimeRange.END_OF_DAY ) {
+      while (startTime < TimeRange.END_OF_DAY) {
 
-            TimeRange unavailableMeetTime = TimeRange.fromStartDuration(startTime, duration);
+          TimeRange potentialMeetTime = TimeRange.fromStartDuration(startTime, duration);
 
-            /** 
-             * This loop goes through all the events and checks if it conflicts with the potential meeting time.
-             * It will add it to a list of unavailble times.
-             * From this I plan to reverse those values and collect the actaul availble meeting times.
-             */
-            for (Event event : events) {
-                TimeRange eventMeetTime = event.getWhen();
-                Collection<String> eventAttendees = event.getAttendees();
-                if (eventMeetTime.overlaps(unavailableMeetTime) == true) {
-                    if(Collections.disjoint(eventAttendees, attendees) == false) {
-                        unavailableTimes.add(unavailableMeetTime);
-                    }
-                } 
-            }
-            startTime = startTime + duration;
+          /** 
+           * This loop goes through all the events and checks if it conflicts with the 
+           * potential meeting time. It will add it to a list of unavailble times.
+           * From this I plan to reverse those values and collect the actaul availble meeting times.
+           */
+          for (Event event : events) {
+            TimeRange eventMeetTime = event.getWhen();
+            Collection<String> eventAttendees = event.getAttendees();
+            if (eventMeetTime.overlaps(potentialMeetTime)) {
+              if (!Collections.disjoint(eventAttendees, attendees)) {
+                potentialTimes.add(potentialMeetTime);
+              }
+            } 
+          }
+          startTime = startTime + duration;
         }
 
-        //Resets startTime for next process of gathering surrounding time blocks
-        startTime = 0;
+      // Resets startTime for next process of gathering surrounding time blocks
+      startTime = 0;
 
-        /**
-         * This function loops through the unavailable time blocks and finds the surrounding
-         * time blocks around it. (AKA the actual available times)
-         */
-        for (TimeRange time : unavailableTimes) {
-            endTime = time.start();
-            //Creates a new Time range with correct
-            TimeRange overlap = TimeRange.fromStartEnd(startTime, endTime, false);
-            if (overlap.duration() >= request.getDuration()) { 
-                availableTimes.add(overlap);
-            }
-            startTime = endTime + time.duration();
+      /**
+       * This function loops through the unavailable time blocks and finds the surrounding
+       * time blocks around it. (AKA the actual available times)
+       */
+      for (TimeRange time : potentialTimes) {
+        endTime = time.start();
+        //Creates a new Time range with correct minutes
+        TimeRange overlap = TimeRange.fromStartEnd(startTime, endTime, false);
+        if (overlap.duration() >= request.getDuration()) { 
+          availableTimes.add(overlap);
         }
+        startTime = endTime + time.duration();
+      }
 
-        //Adds for the last remaining block of time to add to the available times list
-        TimeRange lastTimeBlock =  TimeRange.fromStartEnd(startTime, TimeRange.END_OF_DAY, true);
-        if (lastTimeBlock.duration() >= request.getDuration()) {
-            availableTimes.add(lastTimeBlock);
-        }
-
-        return availableTimes;
+      // Adds for the last remaining block of time to add to the available times list
+      TimeRange lastTimeBlock =  TimeRange.fromStartEnd(startTime, TimeRange.END_OF_DAY, true);
+      if (lastTimeBlock.duration() >= request.getDuration()) {
+        availableTimes.add(lastTimeBlock);
+      }
+      return availableTimes;
     }
 
 }
